eee2aed8cee0b4117d0f9a3a07d3a75f93a9285c
Michael M <m.migsch@gmail.com>
2017-11-16 12:06:25 +0100

    Create evernote_login.py

---
+++ evernote_login.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+
+
+#CREATE DRIVER
+
+driver = webdriver.Firefox()
+
+
+#OPEN URL
+
+driver.get('https://www.evernote.com/Login.action')
+
+
+#LOGIN PROCESS
+
+elem_username = driver.find_element_by_name('username')
+elem_username.send_keys('YOUR EMAIL-ADDRESS')
+elem_username.send_keys(Keys.RETURN)
+sleep(2) #Has to sleep otherwise locating of next element fails.
+elem_password = driver.find_element_by_name('password')
+elem_password.send_keys('YOUR PASSWORD')
+elem_password.send_keys(Keys.RETURN)
